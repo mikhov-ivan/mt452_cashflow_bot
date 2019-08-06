9bf71e1309c31e904f7f56b40b018735b7bb5104
imi <Ivan.Mikhov@techsat.com>
2019-08-06 14:48:18 +0200

    test

--- bot.py
+++ bot.py
@@ -1,54 +1,24 @@
-import requests  
-from bottle import Bottle, response, request as bottle_request
+import telegram
+from telegram.ext import Updater
 
+global bot
+global APP_URL = 'https://mt452-cashflow-bot.herokuapp.com/'
+global APP_KEY = '4276eb911ca3133611d23040e1ee439ea21738932dda94e502f37fae0497'
+global TOKEN = 'bot861062365:AAEq3evcJCE5nZCSclev9Z8ki-cAjwdTUqQ'
 
-class BotHandlerMixin:  
-    BOT_URL = None
+# @app.route('/' + TOKEN, methods=['POST'])
+# def webhook_handler():
+    # if request.method == 'POST':
+        # update = telegram.Update.de_json(request.get_json(force=True))
+        # chat_id = update.message.chat.id
+        # text = update.message.text.encode('utf-8')
+        # bot.sendMessage(chat_id=chat_id, text=text)
+    # return 'ok'
 
-    def get_chat_id(self, data):
-        chat_id = data['message']['chat']['id']
-        return chat_id
+bot = telegram.Bot(token=TOKEN)
 
-    def get_message(self, data):
-        message_text = data['message']['text']
-        return message_text
-    
-    def send_message(self, prepared_data):    
-        message_url = self.BOT_URL + 'sendMessage'
-        requests.post(message_url, json=prepared_data)
-
-
-class TelegramBot(BotHandlerMixin, Bottle):  
-    BOT_URL = 'https://api.telegram.org/bot861062365:AAEq3evcJCE5nZCSclev9Z8ki-cAjwdTUqQ/'
-
-    def __init__(self, *args, **kwargs):
-        super(TelegramBot, self).__init__()
-        self.route('/', callback=self.post_handler, method="POST")
-
-    def change_text_message(self, text):
-        return text[::-1]
-
-    def prepare_data_for_answer(self, data):
-        message = self.get_message(data)
-        answer = self.change_text_message(message)
-        chat_id = self.get_chat_id(data)
-        
-        return {
-            "chat_id": chat_id,
-            "text": answer,
-        }
-
-    def post_handler(self):
-        data = bottle_request.json
-        answer_data = self.prepare_data_for_answer(data)
-        self.send_message(answer_data)
-        return response
-
-
-if __name__ == '__main__':
-    try:
-        app = TelegramBot()
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port, debug=True)
-    except:
-        pass
+updater = Updater(TOKEN)
+PORT = int(os.environ.get('PORT', '8443'))
+updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN, key=APP_KEY)
+updater.bot.set_webhook(APP_URL + TOKEN)
+updater.idle()
