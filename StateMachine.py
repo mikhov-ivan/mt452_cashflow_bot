52abdaf63e7a97e5fdcc2ed4053dd2dc774f76ed
imi <Ivan.Mikhov@techsat.com>
2019-08-07 12:29:24 +0200

    Start state machine

--- StateMachine.py
+++ StateMachine.py
@@ -7,15 +7,16 @@ logger = logging.getLogger()
 
 class State(Enum):
     MAIN = 1
-   
-   
+
+
 class StateMachine:
     def __init__(self):
         self.state = State.MAIN
         self.log_state()
     
     def cmd(self, cmd):
-        pass
+        self.log_state()
     
     def log_state(self):
         logger.info("SM: Current state: {}".format(self.state.name))
+
