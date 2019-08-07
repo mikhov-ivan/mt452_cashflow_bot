4b54e5cf761387ea6b12ea22c7355bab1f45c0fe
imi <Ivan.Mikhov@techsat.com>
2019-08-07 17:18:45 +0200

    Structures: Transaction

--- Cashflow.py
+++ Cashflow.py
@@ -12,11 +12,17 @@ global logger
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
 logger = logging.getLogger()
 
+        
+def send(bot, chat_id, msg):
+    bot.sendMessage(chat_id=chat_id, text=msg, parse_mode='HTML')
+
+def log_update(update):
+    logger.info("{} {}".format(update.update_id, update.message))
+
 
 class CmdPrefix(Enum):
     CATEGORY_GROUP = "cg_"
     CATEGORY = "c_"
-    TRANSACTION = "tr_"
 
 
 class Type(Enum):
@@ -25,45 +31,21 @@ class Type(Enum):
     TRANSACTION = 3
 
 
-class Cashflow:
-    def __init__(self):
-        self.db = DBHelper()
-        self.sm = StateMachine()
-    
-    def set_handlers(self, updater):
-        for prefix in CmdPrefix: updater.dispatcher.add_handler(self.get_regex_handler(prefix))
-        updater.dispatcher.add_handler(CommandHandler("start", self.handle_start))
-        updater.dispatcher.add_handler(CommandHandler("cgs", self.handle_cgs))
-        updater.dispatcher.add_handler(CommandHandler("cats", self.handle_cats))
-        updater.dispatcher.add_handler(CommandHandler("trans", self.handle_trans))
-        
-    def get_regex_handler(self, prefix):
-        if prefix == CmdPrefix.CATEGORY_GROUP: handler = self.handle_category_group
-        elif prefix == CmdPrefix.CATEGORY: handler = self.handle_category
-        return RegexHandler("^(/" + prefix.value + "[a-zA-Z]+)$", handler)
-
-    def handle_category_group(self, bot, update):
-        self.log_update(update)
-        self.send(bot, update.message.chat_id, "Category group: <b>{}</b>".format(update.message.text))
-
-    def handle_category(self, bot, update):
-        self.log_update(update)
-        self.send(bot, update.message.chat_id, "Category: <b>{}</b>".format(update.message.text))
-        
+class GeneralHandler:
     def handle_start(self, bot, update):
         logger.info("User {} {} started bot".format(update.effective_user["id"], update.message.from_user.first_name))
-        self.send(bot, update.message.chat_id, "Hello, <b>{}</b>!".format(update.message.from_user.first_name))
+        send(bot, update.message.chat_id, "Hello, <b>{}</b>!".format(update.message.from_user.first_name))
         
     def handle_cgs(self, bot, update):
-        self.log_update(update)
+        log_update(update)
         self.get_list(Type.CATEGORY_GROUP)
         
     def handle_trans(self, bot, update):
-        self.log_update(update)
+        log_update(update)
         self.get_list(Type.TRANSACTION)
         
     def handle_cats(self, bot, update):
-        self.log_update(update)
+        log_update(update)
         self.get_list(Type.CATEGORY)
         
     def get_list(self, type):
@@ -89,11 +71,33 @@ class Cashflow:
             html = template.format(len(categories), os.linesep, os.linesep, msg)
         else:
             html = "List is empty"
-        self.send(bot, update.message.chat_id, html)
-        
-    def send(self, bot, chat_id, msg):
-        bot.sendMessage(chat_id=chat_id, text=msg, parse_mode='HTML')
+        send(bot, update.message.chat_id, html)
+    
 
-    def log_update(self, update):
-        logger.info("{} {}".format(update.update_id, update.message))
+
+class Cashflow:
+    def __init__(self):
+        self.db = DBHelper()
+        self.sm = StateMachine()
+        self.gh = GeneralHandler()
+    
+    def set_handlers(self, updater):
+        for prefix in CmdPrefix: updater.dispatcher.add_handler(self.get_regex_handler(prefix))
+        updater.dispatcher.add_handler(CommandHandler("start", self.gh.handle_start))
+        updater.dispatcher.add_handler(CommandHandler("cgs", self.gh.handle_cgs))
+        updater.dispatcher.add_handler(CommandHandler("cats", self.gh.handle_cats))
+        updater.dispatcher.add_handler(CommandHandler("trans", self.gh.handle_trans))
+        
+    def get_regex_handler(self, prefix):
+        if prefix == CmdPrefix.CATEGORY_GROUP: handler = self.handle_category_group
+        elif prefix == CmdPrefix.CATEGORY: handler = self.handle_category
+        return RegexHandler("^(/" + prefix.value + "[a-zA-Z]+)$", handler)
+
+    def handle_category_group(self, bot, update):
+        log_update(update)
+        send(bot, update.message.chat_id, "Category group: <b>{}</b>".format(update.message.text))
+
+    def handle_category(self, bot, update):
+        log_update(update)
+        send(bot, update.message.chat_id, "Category: <b>{}</b>".format(update.message.text))
 
