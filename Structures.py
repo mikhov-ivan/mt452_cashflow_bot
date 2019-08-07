0afa88119fe7f55609152730d93d40fc26f0d0e0
imi <Ivan.Mikhov@techsat.com>
2019-08-07 17:53:26 +0200

    GeneralHandler + Structures: Transaction

--- Structures.py
+++ Structures.py
@@ -18,7 +18,7 @@ class Category:
 class Transaction:
     def __init__(self, ouid, execution_date, code, amount, title):
         self.ouid = ouid
-        self.execution_date = datetime.strptime(execution_date, "%Y-%m-%d %H:%M:%S")
+        self.execution_date = datetime.datetime.strptime(execution_date, "%Y-%m-%d %H:%M:%S")
         self.currency = code
         self.amount = amount
         self.title = title
