ed1c195830b7d07226ca870f5daf460d16d3fa0e
imi <Ivan.Mikhov@techsat.com>
2019-08-07 11:15:45 +0200

    MySQL database connector

--- DBHelper.py
+++ DBHelper.py
@@ -26,16 +26,16 @@ class DBHelper:
     def get_categories(self):
         try:
             query = (
-                "SELECT"
-                "   c.ouid AS category_ouid,"
-                "   c.code AS category_code,"
-                "   c_msg.ru AS category_title,"
-                "   cg.ouid AS category_group_ouid,"
-                "   cg_msg.ru AS category_group_title"
-                "FROM category c"
-                "   INNER JOIN msg c_msg ON c_msg.OUID = c.title_msg_ouid"
-                "   INNER JOIN category_group cg ON cg.OUID = c.category_group_ouid"
-                "       INNER JOIN msg cg_msg ON cg_msg.OUID = cg.title_msg_ouid"
+                " SELECT"
+                "    c.ouid AS category_ouid,"
+                "    c.code AS category_code,"
+                "    c_msg.ru AS category_title,"
+                "    cg.ouid AS category_group_ouid,"
+                "    cg_msg.ru AS category_group_title"
+                " FROM category c"
+                "    INNER JOIN msg c_msg ON c_msg.OUID = c.title_msg_ouid"
+                "    INNER JOIN category_group cg ON cg.OUID = c.category_group_ouid"
+                "        INNER JOIN msg cg_msg ON cg_msg.OUID = cg.title_msg_ouid"
             )
             
             response = {}
