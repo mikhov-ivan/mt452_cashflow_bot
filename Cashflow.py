642637f81bf6b3dc585232124b5d29f3dba5bea1
imi <Ivan.Mikhov@techsat.com>
2019-08-07 12:28:00 +0200

    Start state machine

--- Cashflow.py
+++ Cashflow.py
@@ -3,28 +3,12 @@ import logging
 import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from StateMachine import StateMachine
 from DBHelper import DBHelper
-from enum import Enum
 
 global logger
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
 logger = logging.getLogger()
-
-
-class State(Enum):
-    MAIN = 1
-   
-   
-class StateMachine:
-    def __init__(self):
-        self.state = State.MAIN
-        self.log_state()
-    
-    def cmd(self, cmd):
-        pass
-    
-    def log_state(self):
-        logger.info("SM: Current state: {}".format(self.state.name))
 
 
 class Cashflow:
