9cb0000b6eecfcb38d9d1953ed6aeddffea6478a
imi <Ivan.Mikhov@techsat.com>
2019-08-14 14:04:45 +0200

    Commands reworked

--- helpers/DBHelper.py
+++ helpers/DBHelper.py
@@ -63,18 +63,18 @@ class DBHelper:
         if self.mode == "prod":
             try:
                 query = (
-                    " SELECT"
-                    "    c.ouid AS category_ouid,"
-                    "    c.code AS category_code,"
-                    "    c_msg.ru AS category_title,"
-                    "    cg.ouid AS category_group_ouid,"
-                    "    cg.code AS category_group_code,"
-                    "    cg_msg.ru AS category_group_title"
-                    " FROM category c"
-                    "    INNER JOIN msg c_msg ON c_msg.OUID = c.title_msg_ouid"
-                    "    INNER JOIN category_group cg ON cg.OUID = c.category_group_ouid"
-                    "        INNER JOIN msg cg_msg ON cg_msg.OUID = cg.title_msg_ouid"
-                    " WHERE 1 = 1 {}"
+                    " SELECT" +
+                    "    c.ouid AS category_ouid," +
+                    "    c.code AS category_code," +
+                    "    c_msg.ru AS category_title," +
+                    "    cg.ouid AS category_group_ouid," +
+                    "    cg.code AS category_group_code," +
+                    "    cg_msg.ru AS category_group_title" +
+                    " FROM category c" +
+                    "    INNER JOIN msg c_msg ON c_msg.OUID = c.title_msg_ouid" +
+                    "    INNER JOIN category_group cg ON cg.OUID = c.category_group_ouid" +
+                    "        INNER JOIN msg cg_msg ON cg_msg.OUID = cg.title_msg_ouid" +
+                    " WHERE 1 = 1 {}" +
                     " ORDER BY c_msg.ru")
                 
                 where = ""
